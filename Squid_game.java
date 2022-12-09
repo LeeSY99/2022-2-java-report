20c08d88038558c0411757592a046171186ebbad
이상윤 <101381138+LeeSY99@users.noreply.github.com>
2022-12-09 14:27:24 +0900

    Delete Squid_game.java

--- Squid_game.java
+++
@@ -1,9 +0,0 @@
-package korea;
-
-public class Squid_game implements Drama{
-	public void print() {
-		System.out.println("오징어게임:");
-		System.out.println("넷플릭스 오리지널 한국 드라마, 6주동안 넷플릭스 글로벌 1위를 하는 등 넷플릭스 TV 프로그램 역대 시청 가구수 순위 압도적 1위");
-		System.out.println("콘텐츠 공개 수 28일간 시청 시간 1,650,450,000시간로 1위를 차지하면서 세계적으로 오징어게임 열풍을 이끔.");
-	}//interface 매개변수 다형성 K_drama_explain.java
-}
